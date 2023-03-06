b6da5273acad484f0ea1a5188707ac4f97048dc7
이정운 <aronlee5263@gmail.com>
2023-03-06 20:43:05 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,18 +1,22 @@
 function solution(price) {
     
-let discountRate;
+    let discountRate;
     
-    if (price >= 500000) {
+    if(price >= 500000){
         discountRate = 0.2;
-    } else if (price >= 300000) {
+    }
+    else if(price >= 300000){
         discountRate = 0.1;
-    } else if (price >= 100000) {
+    }
+    else if(price >= 100000){
         discountRate = 0.05;
-    } else {
+    }
+    else {
         discountRate = 0;
     }
     
-    const discountedPrice = price * (1 - discountRate);
+    const discountedPrice = price * (1 - discountRate)
+    return Math.floor(discountedPrice)
     
-    return Math.floor(discountedPrice);
+    
 }
