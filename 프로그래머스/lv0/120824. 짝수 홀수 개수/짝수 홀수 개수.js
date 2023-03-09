a8bfd3f09c166adf8f30e4729372dc32a478e4c6
이정운 <aronlee5263@gmail.com>
2023-03-09 18:03:26 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,21 +1,17 @@
 function solution(num_list) {
-    
-    let even = 0;
+  
+    let even = 0
     let odd = 0;
-    let cnt = 0;
+    let length = num_list.length
     
-    while(cnt < num_list.length){
+    for(let cnt =0; cnt <length; cnt ++){
+        
         if(num_list[cnt] % 2 === 0){
-            even = even + 1            
+            even = even + 1
         }
         else{
-            odd = odd + 1;
+            odd = odd + 1
         }
-        
-        cnt = cnt + 1
-    }
-    
-    
-    
-    return [even, odd];
+    }    
+    return [even, odd]   
 }
