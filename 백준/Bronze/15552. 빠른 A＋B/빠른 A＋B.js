d6b96e30b0cc78b8da0edfe0552e1b3344544adb
이정운 <aronlee5263@gmail.com>
2023-12-21 18:53:05 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1624 ms, Memory: 171924 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -0,0 +1,17 @@
+
+let fs = require("fs")
+let input = fs.readFileSync("/dev/stdin").toString().split("\n")
+
+let n = Number(input[0])
+
+let result =""
+for(let i=1; i <= n; i++){
+    let a = Number(input[i].split(" ")[0])
+    let b = Number(input[i].split(" ")[1])
+
+    let accumulatedValue = a + b 
+    result += accumulatedValue;
+    result += "\n"
+}
+
+console.log(result)
