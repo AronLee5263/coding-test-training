6204360677dbb003243b4d124b89ab24fd818725
이정운 <aronlee5263@gmail.com>
2023-12-27 12:18:58 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 128 ms, Memory: 9728 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.js
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.js
@@ -1,21 +1,22 @@
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let n = Number(input[0])
+let testCases = Number(input[0])
 
-for(let i = 1; i<=n; i++){
-    let classStudentsScore = input[i].split(" ").map(Number)
-    let n = classStudentsScore[0]
+for(let i=1; i <= testCases; i++){
+    let data = input[i].split(" ").map(Number)
+    let n = data[0];
     let summary = 0;
-
-    for(let k=1; k <= n; k++){
-        summary += classStudentsScore[k]
+    for(let j=1; j <=n; j++){
+        summary += data[j]
     }
-    let average = summary / n;
-    let cnt = 0;
-    for(let i=1; i<=n; i++){
-    if(classStudentsScore[i] > average) cnt += 1;
+    let average = summary / n
+    let studentCnt = 0;
+    for(let k =1; k <=n; k++){
+        if(average < data[k]){
+            studentCnt += 1
+        }
     }
-    console.log(`${(cnt / n * 100).toFixed(3)}%`)
+    console.log(`${(studentCnt / n * 100).toFixed(3)}%`)
+    
 }
-               
